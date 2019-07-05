93c0bfb7a397a942bba7dcb230c39d492efa54d3
xiaomessi <921721870@qq.com>
2019-07-05 23:36:44 +0800

    a

---
+++ Pair.java
@@ -0,0 +1,35 @@
+package com.fehead.pair;
+
+public class Pair<String> {
+    private String first;
+    private String second;
+
+
+    public Pair(){
+        first = null;
+        second = null;
+    }
+
+    public Pair(String first,String second){
+        this.first = first;
+        this.second = second;
+    }
+
+
+    public String getFirst(){
+        return first;
+    }
+
+    public String getSecond(){
+        return second;
+    }
+
+
+    public void setFirst(String newValue){
+        first = newValue;
+    }
+
+    public void setSecond(String newValue){
+        second = newValue;
+    }
+}
