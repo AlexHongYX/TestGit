c4c7c766a4f5d36260078c15acc1427dfce57027
xiaomessi <921721870@qq.com>
2019-12-24 23:19:52 +0800

    add

---
+++ WindowTest.java
@@ -0,0 +1,18 @@
+package SelectSystem;
+
+import java.awt.Color;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+
+
+public class WindowTest extends WindowAdapter 
+{
+	public void windowOpened(WindowEvent e)
+	{
+		JFrame frame=(JFrame)e.getSource();
+		frame.getContentPane().setBackground(Color.white);
+	}
+}
+
